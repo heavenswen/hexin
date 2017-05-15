cfeef6817b4a8f5ec5aa1238b9037b1fa154d918
Heavenswen <423822728@qq.com>
2017-05-15 17:34:04 +0800

    hexin

---
+++ src/pages/user/index/app.js
@@ -0,0 +1,12 @@
+import "animate.css/animate.min.css"
+import 'assets/css/react.scss'
+import 'assets/css/index.scss'
+//rem 
+import rem from "assets/js/rem.js"
+(function () {
+  //rem init
+  new rem();
+
+
+
+})()
